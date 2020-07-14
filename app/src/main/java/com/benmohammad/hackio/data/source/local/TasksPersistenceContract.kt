e563ea869b0aed29483a5ffbdb2c70034b214e75
Ben Mohammad <benmohammad593@gmail.com>
2020-07-14 07:06:29 +0100

    TaskDbHelper added...

--- app/src/main/java/com/benmohammad/hackio/data/source/local/TasksPersistenceContract.kt
+++ app/src/main/java/com/benmohammad/hackio/data/source/local/TasksPersistenceContract.kt
@@ -6,7 +6,7 @@ object TasksPersistenceContract {
 
     object TaskEntry: BaseColumns {
         const val TABLE_NAME = "tasks"
-        const val NAME_ENTRY_ID = "entryId"
+        const val COLUMN_NAME_ENTRY_ID = "entryId"
         const val COLUMN_NAME_TITLE = "title"
         const val COLUMN_NAME_DESCRIPTION = "description"
         const val COLUMN_NAME_COMPLETED = "completed"
