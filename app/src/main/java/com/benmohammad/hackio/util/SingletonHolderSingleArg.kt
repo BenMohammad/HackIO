087b19a966a19f9b74cd29e09cb86e0b8dfbd326
Ben Mohammad <benmohammad593@gmail.com>
2020-07-15 21:53:34 +0100

    utils SingletonHolderSingleArg.kt added

---
+++ app/src/main/java/com/benmohammad/hackio/util/SingletonHolderSingleArg.kt
@@ -0,0 +1,23 @@
+package com.benmohammad.hackio.util
+
+open class SingletonHolderSingleArg<out T, in A>(creator: (A) -> T) {
+    private var creator: ((A) -> T)? = creator
+    @Volatile private var instance: T? = null
+
+    fun getInstance(arg: A): T {
+        val i = instance
+        if(i != null) {
+            return i
+        }
+        return synchronized(this) {
+            val i2 = instance
+            if(i2 != null) {
+                i2
+            } else {
+                val created = creator!!(arg)
+                instance = created
+                created
+            }
+        }
+    }
+}
